6ceeba2a66917dbc6797ed8c9b769b9ab59a9881
928rino@gmail.com <928rino@gmail.com@7b327a80-43ac-6113-e5da-e3b55c5032a6>
2010-04-06 13:25:24 +0000

    Share project

---
+++ trunk/VogellaEditor/src/de/vogella/rcp/intro/edtor/Perspective.java
@@ -0,0 +1,16 @@
+package de.vogella.rcp.intro.edtor;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+		String editorArea = layout.getEditorArea();
+		layout.setEditorAreaVisible(true);
+		layout.setFixed(true);
+		
+		layout.addStandaloneView(View.ID,  false, IPageLayout.LEFT, 1.0f, editorArea);
+	}
+
+}
